8a3a4cd6980eec32cb759f0f0163c0799868dda6
Chris Cools <chrisc@pomax.com>
2018-11-28 20:59:01 +0100

    huiswerk

---
+++ Homework 7.3 secret number/secret number.py
@@ -0,0 +1,12 @@
+# code om een nummer te raden
+# het te raden nummer eerst vastzetten in variabele secret
+secret = 27
+# opdracht geven om te raden naar het nummer
+guess = int (raw_input("Welk getal tussen 1 en 30 zit er in mijn hoofd?"))
+
+#evalueren of de gok gelijk is aan het ingestelde getal
+if secret == guess:
+    print ("Goed gegokt, misschien  mee doen de lotto?")
+
+else :
+    print ("Je mag 3 keer raden voor 12 euro!!")
